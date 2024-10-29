8a9a65300ac6a754aa595102169c55f3c4ff53b1
Pieter Neerincx <pieter.neerincx@gmail.com>
2024-10-29 22:42:44 +0100

    Simplified code and added cleanup of instance specific sinfo report file.

--- roles/slurm/templates/notification_scripts/nodes.bash
+++ roles/slurm/templates/notification_scripts/nodes.bash
@@ -8,7 +8,7 @@ set -o pipefail
 #
 node_state_dir='/var/run/slurm/'
 node_state_file_old="${node_state_dir}/sinfo_report_old"
-node_state_file_new="${node_state_dir}/sinfo_report_new"
+node_state_file_new="${node_state_dir}/sinfo_report_new-${$}"
 send_notification='false'
 
 #
@@ -53,7 +53,7 @@ thereShallBeOnlyOne "${node_state_dir}/nodes.lock"
 # regular*|up|9|mixed|gs-vcompute[01-04,06-10]|none
 # user_interface|up|1|idle|gearshift|none
 #
-sinfo -o "%P|%a|%D|%T|%N|%E" > "${node_state_file_new}-${$}"
+sinfo -o "%P|%a|%D|%T|%N|%E" > "${node_state_file_new}"
 
 #
 # Compile JSON node state message payload.
@@ -63,12 +63,12 @@ read -r -d '\0' message <<- EOM
 		"type": "mrkdwn",
 		"text": "*Node state on _{{ slurm_cluster_name | capitalize }}_ changed*:  
 		\`\`\`
-		$(tr \" \' < "${node_state_file_new}-${$}" | column -t -s '|')
+		$(tr \" \' < "${node_state_file_new}" | column -t -s '|')
 		\`\`\`"
 	}\0
 EOM
 
-#logger --id="${$}" -s "Got new node state stored in ${node_state_file_new}-${$}."
+#logger --id="${$}" -s "Got new node state stored in ${node_state_file_new}."
 
 #
 # Delete old node state after one day to send a new notification
@@ -90,16 +90,16 @@ if [[ -e "${node_state_file_old}" ]]; then
 		# E.g. load was too high and changed slightly, but is still is too high.
 		#
 		node_state_file_old_short="${node_state_file_old}-${$}.short"
-		node_state_file_new_short="${node_state_file_new}-${$}.short"
+		node_state_file_new_short="${node_state_file_new}.short"
 		sed 's/:  [^:]*$//' "${node_state_file_old}" | sort > "${node_state_file_old_short}"
-		sed 's/:  [^:]*$//' "${node_state_file_new}-${$}" | sort > "${node_state_file_new_short}"
+		sed 's/:  [^:]*$//' "${node_state_file_new}" | sort > "${node_state_file_new_short}"
 		#logger --id="${$}" -s "Created ${node_state_file_old_short} and ${node_state_file_new_short}."
 		if cmp -s "${node_state_file_old_short}" "${node_state_file_new_short}"; then
 			: # no-op: The files are identical: nothing changed and there is no need to send a new notification.
 			#logger --id="${$}" -s "No differences found in ${node_state_file_old_short} compared to ${node_state_file_new_short}."
 		else
 			#logger --id="${$}" -s "Differences were found in ${node_state_file_old_short} compared to ${node_state_file_new_short}."
-			cp -v "${node_state_file_new}-${$}" "${node_state_file_old}"
+			cp -v "${node_state_file_new}" "${node_state_file_old}"
 			send_notification='true'
 			#logger --id="${$}" -s "send_notification = ${send_notification}."
 		fi
@@ -120,7 +120,7 @@ if [[ -e "${node_state_file_old}" ]]; then
 	fi
 else
 	#logger --id="${$}" -s "${node_state_file_old} does not exist: send notification."
-	cp -v "${node_state_file_new}-${$}" "${node_state_file_old}"
+	cp -v "${node_state_file_new}" "${node_state_file_old}"
 	send_notification='true'
 	#logger --id="${$}" -s "send_notification = ${send_notification}."
 fi
@@ -137,5 +137,10 @@ else
 	: # no-op
 	#logger --id="${$}" -s 'Node state has not changed: not sending message.'
 fi
+
+#
+# Cleanup
+#
+rm -v -f "${node_state_file_new}"
 
 exit 0
