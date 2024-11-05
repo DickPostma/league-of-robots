174fda1547bb19e7f6d4f08dd863287c6e2b0f9a
Pieter Neerincx <pieter.neerincx@gmail.com>
2024-11-05 16:16:05 +0100

    Improved roles/cleanup_gs_data/templates/cleanup_gs.bash based on review comments.

--- roles/cleanup_gs_data/templates/cleanup_gs.bash
+++ roles/cleanup_gs_data/templates/cleanup_gs.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
 slurm_notification_slack_webhook='{{ slurm_notification_slack_webhook }}'
 {% raw %}
 dirToCheck="/groups/umcg-genomescan/"
-dateInSecNow=$(date +%s)
+currentTimeInSeconds="$(date +%s)"
 
 #
 # Find all GenomeScan batch dirs ignoring files.
@@ -33,23 +37,34 @@ dateInSecNow=$(date +%s)
 #     104832-210/Raw_data/
 #     104832-210/UMCG_CSV_104832-210.csv
 #
-for dir in $(find "${dirToCheck}" -mindepth 1 -maxdepth 1 -type d | sort)
+if [[ -e "${dirToCheck}" && -r "${dirToCheck}" ]]
+then
+	: #no-op
+else
+	echo "FATAL: ${dirToCheck} does not exist or is not readable."
+	exit 1
+fi
+readarray -t batchDirs < <(find "${dirToCheck}" -mindepth 1 -maxdepth 1 -type d | sort)
+if [[ "${#batchDirs[@]}" -eq '0' ]]
+then
+	echo "No batch dirs found at ${dirToCheck}."
+	exit 0
+fi
+for batchDir in "${batchDirs[@]}"
 do
-	echo "Processing ${dir} ..."
+	echo "Processing ${batchDir} ..."
 	#
-	# Check creation time instead of modification time
-	# and remove /groups from dir for lookup with debugfs.
+	# Check creation time instead of modification time.
 	#
-	creationTime=$(/sbin/debugfs -R 'stat '${dir#*/*/} /dev/vdb | awk '/crtime/{print $2}' FS='--')
-	creationTimeSeconds=$(date -d"${creationTime}" +%s)
+	creationTimeInSeconds="$(stat -c '%W' "${batchDir}")"
 	echo "  Checking age ..."
-	if [[ $(((${dateInSecNow} - ${creationTimeSeconds}) / 86400)) -gt 14 ]]
+	if [[ $(((${currentTimeInSeconds} - ${creationTimeInSeconds}) / 86400)) -gt 14 ]]
 	then
 		echo "  Deleting folder, because it is older than 14 days ..."
-		rm -rf "${dir}"
+		#rm -rf "${batchDir}"
 	else
 		echo "  Keeping folder, because it is not yet older than 14 days."
-		foundUnprocessedData="$(find "${dir}" -mindepth 1 -maxdepth 2 \
+		foundUnprocessedData="$(find "${batchDir}" -mindepth 1 -maxdepth 2 \
 				! -name Raw_data -a \
 				! -name Analysis -a \
 				! -name '*.finished' -a \
@@ -60,10 +75,10 @@ do
 			echo "    Folder empty or contains only leftovers from processed data."
 		else 
 			echo "    Folder contains unprocessed data: checking age ..."
-			if [[ $(((${dateInSecNow} - ${creationTimeSeconds}) / 86400)) -gt 7 ]]
+			if [[ $(((${currentTimeInSeconds} - ${creationTimeInSeconds}) / 86400)) -gt 7 ]]
 			then
 				echo "      Unprocessed folder is older than 7 days: sending warning ..."
-				creationDate=$(date -d "${creationTime}")
+				creationDate="$(date -d "@${creationTimeInSeconds}")"
 				deletionDate=$(date -d "${creationDate} +15 days")
 				#
 				# Compile JSON message payload.
@@ -71,9 +86,9 @@ do
 				read -r -d '\0' message <<- EOM
 					{
 						"type": "mrkdwn",
-						"text": "*Cleanup alert for _$(hostname):${dir}_*:
+						"text": "*Cleanup alert for _$(hostname):${batchDir}_*:
 						\`\`\`
-						This unprocessed data is older than a week and will be *deleted on ${delete_date}*!
+						This unprocessed data is older than a week and will be *deleted on ${deletionDate}*!
 						\`\`\`"
 					}\0
 				EOM
